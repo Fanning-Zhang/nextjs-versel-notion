310ef53cbc34d269b0d09670f0b3e8e1c9b1142a
fanning <hl_zhang@tju.edu.cn>
2023-04-24 21:23:10 +0800

    update the linkedin

--- site.config.ts
+++ site.config.ts
@@ -19,7 +19,7 @@ export default siteConfig({
   // social usernames (optional)
   twitter: 'Fanning_Hz',
   github: 'Fanning-Zhang',
-  linkedin: 'fisch2',
+  linkedin: '慧林-张-62382025b',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
